473e5e538abf5eb538ef251ff0dd1837748e9140
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-08-05 01:19:51 -0500

    ADD: New changes

--- web-app/src/app/components/todo-card/todo-card.component.ts
+++ web-app/src/app/components/todo-card/todo-card.component.ts
@@ -17,6 +17,7 @@ export class TodoCardComponent implements OnInit {
   tasks: TaskInterface[] = [];
   selectedTag: string = 'All'
   uniqueTasks: TaskInterface[] = [];
+  filteredTasks: TaskInterface[] = [];
 
   constructor(private taskService: TasksService) {}
 
@@ -39,12 +40,31 @@ export class TodoCardComponent implements OnInit {
 
         this.uniqueTasks = uniqueTasks;
         this.tasks = tasks;
+        this.filteredTasks = this.tasks;
+        this.filterTasks();
       },
       error: (error) => {
         console.error('Error fetching tasks:', error);
       }
     });
   }
+
+  filterTasks() {
+    console.log(this.selectedTag)
+
+    if (this.selectedTag === 'All') {
+      this.filteredTasks = this.tasks;
+    } else {
+      this.filteredTasks = this.tasks.filter(task => task.tag === this.selectedTag);
+    }
+
+    console.log(this.filterTasks)
+  }
+
+  toggleTaskCompletion(task: TaskInterface) {
+    task.completed = !task.completed;
+  }
+
 
 }
 
